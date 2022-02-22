12458cf154be6c660481d94fdabc983b05c6eaf3
Eric <erichao2018@gmail.com>
2022-02-21 16:50:29 -0800

    added minecount and board visualization (#10)

--- server/minesweeper.js
+++ server/minesweeper.js
@@ -12,39 +12,73 @@ class Board {
       throw `Impossible to populate ${n} mines in a ${this.height} by ${this.width} board.`
     }
 
-    this.mineCount = n
-
-    const mineLocations = []
-    while (mineLocations.length < n) {
+    this.mineLocations = []
+    while (this.mineLocations.length < n) {
       const candidate = Math.floor(Math.random() * this.height * this.width)
-      if (!mineLocations.includes(candidate)) {
-        mineLocations.push(candidate)
+      if (!this.mineLocations.includes(candidate)) {
+        this.mineLocations.push(candidate)
       }
     }
 
-    console.log(mineLocations)
-
-    mineLocations.forEach((ml) => {
-      const r = Math.floor(ml / this.height)
+    this.mineLocations.forEach((ml) => {
+      const r = Math.floor(ml / this.width)
       const c = ml % this.width
 
-      this.board[r][c] = new Tile(true)
+      this.board[r][c].setContainsMine(true)
+
+      for (let rr = r - 1; rr <= r + 1; rr++) {
+        for (let cc = c - 1; cc <= c + 1; cc++) {
+          if (rr === r && cc === c) {
+            continue
+          }
+
+          if (rr < 0 || cc < 0 || rr >= this.height || cc >= this.width) {
+            continue
+          }
+
+          this.board[rr][cc].incrementSurroundingMineCount()
+        }
+      }
     })
   }
 
-  toString() {
+  toDetailedString() {
     const stringLines = [
       'Board',
       `height:${this.height}`,
       `width:${this.width}`,
-      `mineCount:${this.mineCount}`,
+      `mineLocations:${this.mineLocations}`,
       `tiles:`,
     ]
 
     this.board.forEach((row) => {
-      stringLines.push(`[${row.map((t) => t.toString()).join(',\n')}]\n`)
+      stringLines.push(
+        `[${row.map((t) => t.toDetailedString()).join(',\n')}]\n`,
+      )
     })
 
+    return stringLines.join('\n')
+  }
+
+  toString() {
+    const horizontalBorder = '-'.repeat(this.width * 2 + 1)
+    const stringLines = [horizontalBorder]
+
+    this.board.forEach((row) => {
+      const currentRow = []
+
+      row.forEach((tile) => {
+        if (tile.containsMine) {
+          currentRow.push('x')
+        } else {
+          currentRow.push(tile.surroundingMineCount)
+        }
+      })
+
+      stringLines.push('|' + currentRow.join('|') + '|')
+    })
+
+    stringLines.push(horizontalBorder)
     return stringLines.join('\n')
   }
 }
@@ -53,15 +87,25 @@ class Tile {
   constructor(containsMine) {
     this.containsMine = containsMine
     this.isRevealed = false
+    this.surroundingMineCount = 0
+  }
+
+  incrementSurroundingMineCount() {
+    this.surroundingMineCount++
+  }
+
+  setContainsMine(containsMine) {
+    this.containsMine = containsMine
+  }
+
+  toDetailedString() {
+    return `[Tile containsMine:${this.containsMine} isRevealed:${this.isRevealed} surroundingMineCount:${this.surroundingMineCount}]`
   }
 }
 
-Tile.prototype.toString = function () {
-  return `[Tile containsMine:${this.containsMine} isRevealed:${this.isRevealed}]`
-}
-
 if (require.main === module) {
-  const board = new Board(4, 4)
-  board.randomlyPopulateMines(5)
+  const board = new Board(10, 50)
+  board.randomlyPopulateMines(50)
+  // console.log(board.toDetailedString())
   console.log(board.toString())
 }
