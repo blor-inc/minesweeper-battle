13fd4c6ea21aa831aa55be1d4be0071bc8788161
Eric <erichao2018@gmail.com>
2022-02-22 21:23:48 -0800

    connect front and back end with some basic APIs (#11)

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,43 @@
-import React from "react";
-import logo from "./logo.svg";
-import "./App.css";
+import React from 'react'
+import './App.css'
 
 function App() {
-  const [data, setData] = React.useState(null);
+  const [data, setData] = React.useState(null)
+  const [gameState, setGameState] = React.useState(null)
 
   React.useEffect(() => {
-    fetch("/api")
+    fetch('/debug')
       .then((res) => res.json())
-      .then((data) => setData(data.message));
-  }, []);
+      .then((data) => setData(data.message))
+  }, [])
+
+  function newGame() {
+    fetch('/new-game', { method: 'POST' })
+      .then((res) => res.json())
+      .then((data) => {
+        setGameState(data)
+
+        console.log(data)
+      })
+  }
+
+  function populateGameBoard(gameState) {
+    console.log(gameState)
+    // TODO
+    return <div>Display Board Here</div>
+  }
 
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>{!data ? "Loading..." : data}</p>
+        <p>{!data ? 'Loading...' : data}</p>
+        <button hidden={gameState != null} onClick={newGame}>
+          New Game
+        </button>
+        {gameState != null && populateGameBoard(gameState)}
       </header>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
