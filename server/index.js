dcaf63e2351734e39fdac650a002bc738878d5f8
Derzan Chiang <derzanchiang1800@gmail.com>
2022-03-03 01:20:20 -0800

    please

--- server/index.js
+++ server/index.js
@@ -6,7 +6,7 @@ const path = require('path');
 
 const minesweeper = require('./minesweeper');
 
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 3000;
 const app = express();
 
 const httpServer = createServer(app);
